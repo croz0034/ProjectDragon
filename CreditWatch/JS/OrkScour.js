53fc3744dadd73898173296356721289aeec6f57
croz0034 <32398078+croz0034@users.noreply.github.com>
2019-02-15 02:28:18 -0500

    first commit

---
+++ CreditWatch/JS/OrkScour.js
@@ -0,0 +1,66 @@
+let CreditWatch = {
+    parks: "",
+    people: [],
+    init: () => {
+        let c = CreditWatch;
+        c.parkPopulate(document.getElementById("Park"))
+    },
+    parkPopulate: (zone) => {
+        //The northern empire is kingdom 31
+        jsork.kingdom.getParks(31, (data) => {
+            data.forEach((park) => {
+                zone.innerHTML += `<option value="${park.ParkId}">${park.Name}</option>`;
+            })
+        });
+        zone.addEventListener("change", CreditWatch.peoplePopulate)
+    },
+    peoplePopulate: (ev) => {
+        let zone = document.getElementById("textOutput")
+        jsork.park.getPlayers(ev.target.value, 0, function (data) {
+            data.forEach((Player) => {
+                CreditWatch.people.push({
+                    "Name": Player.Persona,
+                    "ID": Player.MundaneId
+                });
+            });
+            CreditWatch.Counter = CreditWatch.people.length;
+            for (i = 0; i < CreditWatch.people.length; i++) {
+                CreditWatch.personData(CreditWatch.people[i]);
+            }
+            document.getElementById("ClassFilter").addEventListener("change", CreditWatch.Display);
+        });
+
+    },
+    personData: (person) => {
+        jsork.player.getClasses(person.ID, function (data) {
+            person.credits = data;
+            CreditWatch.Counter--;
+            if (CreditWatch.Counter == 0) {
+                let RD = document.getElementById("ReadyIcon");
+                RD.style.background = "lightgreen";
+                RD.textContent = "Ready"
+            }
+        })
+    },
+    Display: (ev) => {
+        let zone = document.getElementById("Content")
+        let levels = [[0, 5], [5, 12], [12, 21], [21, 34], [34, 53], [53, 999]];
+        let job = document.getElementById("ClassFilter").value;
+        let threshHold = document.getElementById("TillNext").value;
+        zone.innerHTML = "";
+
+        CreditWatch.people.forEach((person) => {
+            for (i = 0; i < person.credits.length; i++) {
+                if (person.credits[i].class == job) {
+                    let TilNext = levels[(person.credits[i].level - 1)][1] - (person.credits[i].credits)
+                    if (TilNext < threshHold) {
+                        console.log(`Person: ${person.Name} Credits: ${person.credits[i].credits} Required: ${levels[person.credits[i].level-1][1]} threshHold: ${threshHold}`)
+                        zone.innerHTML += `<p> ${person.Name}: ${TilNext} </p>`
+                    }
+                }
+            }
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', CreditWatch.init)
---
+++ SpellBook/JS/SpeechAndBluetooth.js
@@ -0,0 +1,102 @@
+let initAll = ()=>{
+    speechModule.init();
+    earModule.init()
+}
+let speechModule = {
+    synth: "",
+    init: (ev)=>{
+        let s = speechModule;
+        s.synth = window.speechSynthesis;
+    },
+    Parrot: (ev)=>{
+        console.log(ev.target);
+        let WhatToSay;
+        if(ev.target){ WhatToSay = new SpeechSynthesisUtterance(ev.target.textContent) }
+        else {
+        WhatToSay = new SpeechSynthesisUtterance("I can't let you do that, Dave");
+        }
+        speechModule.synth.speak(WhatToSay);
+    },
+    listen: (ev)=>{
+        let s = speechModule;
+        
+    }
+}
+let earModule = {
+    // Recognition stuff
+    SpeechRecognition: webkitSpeechRecognition,
+    SpeechGrammarList: webkitSpeechGrammarList,
+    SpeechRecognitionEvent: webkitSpeechRecognitionEvent,
+    recognition: "",
+    grammar: '#JSGF V1.0; grammar Swears; public <swear> = awe ;',
+    
+    init: ()=>{
+        document.getElementById("textOutput").addEventListener('click', speechModule.Parrot)
+        let e = earModule;
+        e.recognition = new e.SpeechRecognition();
+var speechRecognitionList = new e.SpeechGrammarList();
+        
+        speechRecognitionList.addFromString(e.grammar, 0);
+        
+        e.recognition.grammars = speechRecognitionList;
+        e.recognition.continuous = true;
+        e.recognition.lang = 'en-US';
+        e.recognition.interimResults = false;
+        e.recognition.maxAlternatives = 5;
+        
+         document.getElementById("Microphone").addEventListener('click', e.listen)
+        
+    },
+    listen: ()=>{
+        earModule.recognition.start();
+        let drop = document.getElementById("Microphone")
+        drop.classList.add("active");
+        drop.textContent = "Listening";
+        earModule.recognition.onresult = function(event) {
+            let result = event.results[(event.results.length -1)][0].transcript.toLowerCase();
+            console.log(event.results[(event.results.length -1)])
+            console.log(result)
+            
+            for(i=0; i < abilities.length; i ++){
+                let spell = abilities[i];
+               if(result.includes(spell.name.toLocaleLowerCase())){
+                   
+                   spell.type = spellType[spell.type];
+                   spell.school = spellSchool[spell.school];
+                   spell.incant = `"${spell.incant[0]}" X ${spell.incant[1]}`;
+                   spell.range = spellRange[spell.range];
+                   spell.materials = spell.materials[0] + spellMaterials[spell.materials[1]];
+                   
+               let c = earModule.ContentFill;
+                   document.querySelectorAll(".OverZone").forEach((Element)=>{
+                       c(Element.id, spell)
+                   })
+                   
+               
+                   return "done"
+               } else { console.log("nope")}
+            } 
+              
+    }
+        earModule.recognition.onend = ()=>{
+        drop.classList.remove("active");
+        drop.textContent = "Begin";
+        }
+},
+    ContentFill: (target, spell)=>{
+        let targetzone = document.querySelector(`#${target}`)
+        console.log(targetzone.childNodes)
+        if(spell[target]){
+            targetzone.classList.remove("hidden")
+            console.log(spell[target])
+            if(Array.isArray(spell[target])){spell[target] = spell[target].join("<br>");
+                                            
+            console.log(spell[target])}
+            targetzone.querySelector(".Mutable").innerHTML = spell[target]}
+        
+        else{ targetzone.classList.add("hidden")}
+    }
+}
+ 
+document.addEventListener("DOMContentLoaded", initAll)
+
