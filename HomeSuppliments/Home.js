5e30389d902e62cd9f8497b8147d901964a9880e
croz0034 <32398078+croz0034@users.noreply.github.com>
2019-02-18 01:58:58 -0500

    Loading bar tips added

--- HomeSuppliments/Home.js
+++ HomeSuppliments/Home.js
@@ -1,6 +1,3 @@
-//const tryel = {name: "Tryel", ID:59484}
-//const person = "#name=Tryel&&ID=59484";
-//#name=Skye&&ID=59483
 let HomeData = {
         DataReturned: false,
         user: {
@@ -18,6 +15,7 @@ let HomeData = {
             window.location.href.includes("#") ? HomeData.TranslateGet() :
                 console.log('nada');
             Navigation.init();
+            document.querySelector("#Tips").textContent = Tips[Math.floor(Math.random() * 20)];
         },
         TranslateGet: (ev) => {
             let url = window.location.href;
@@ -224,12 +222,13 @@ let HomeData = {
                     page.classList.add("hidden")
                     let additions = document.createElement("button");
                     additions.textContent = page.classList[1];
-                    additions.style.left = `${10 * x}vh`;
                     additions.addEventListener('click', (ev) => {
                         Navigation.pages.forEach((page) => {
                             page.classList.add("hidden")
                         });
                         page.classList.remove("hidden")
+                        
+            document.querySelector("#Tips").textContent = Tips[Math.floor(Math.random() * 20)];
                     });
                     document.querySelector("nav").appendChild(additions);
                     x++;
@@ -239,5 +238,27 @@ let HomeData = {
 
             }
         }
+const Tips = [
+"Throw your shield at engulfing spell balls to block their effect (p19)", "A stopped player may not use blink, teleport, or be affected by lost or banish (p52)",
+"Extraordinary abilities (marked with (ex)) do not require a free hand and are not stopped by pro-mag or void touched (p48)",
+"Force bolts will still destroyed hardened weapons, or monks below the level of 6 (p59, 61)", 
+"Undead minions are tricky to deal with. If you can't kill them, kill their healer. Doing so will stop them from coming back (p59, 66)", 
+"Sanctuary, and insubstantial stop the player who is using them from (impeding play) and (physically interracial with other players) respectively. (P52, 63)", 
+"You may use release on hostile players as long as they are incapable of moving or engaging in combat… remember this when someone uses shadow step (p50)", 
+"A dead leg is only an invalid target while it's knee is in contact with the ground or during its initial placement (p6)", 
+"players who move after dieing may not be resurrected (p7). Unless they are summoned first (p65)", 
+"If a weapon or shield is destroyed, you may repair it after 60 seconds at base (p24)", 
+"Even if your opponent is wounded or killed, you aren't safe yet! Any shot they were throwing still has a chance to connect within a ½ second (p7)", 
+"(Ex) extraordinary enchantments don't take up your enchant slots, or interfere with (m) enchantments in any way (p29, 48)", 
+"If you want an attuned from a druid, find them a scout with evolution. Most druids won't waste the points on a spell they don't intend to use (p57)", 
+"Teleport and lost only force movement as long as the effected player is insubstantial. Use tracking or shake it off creatively (p60,66)", 
+"Great weapons with 18” of heavy padding (4” cross section) gain the armour breaking and shield crushing traits regardless of class using them. (P18)", 
+"Gift of air is susceptible to magic, or melee armour breaking or shield crushing. Consult your local Barbarian (p33, 58)", 
+"All effects from 'poison’ are considered magical. A protection from magic, or void touched could save your life (p49)", 
+"you can frozen or insubstantial to become immune to more dangerous spells, lord T calls this 'shadow dancing’ (p51, 52)", 
+"magic requires a free hand to cast. Luckily a small shield does not need to be held in hand to be wielded (p19, 49)", 
+"Fight after death, elemental barrage, and sanctuary are all ongoing effects. In the right circumstances a greater / release can do wonders (p51)", 
+
+];
 
         document.addEventListener("DOMContentLoaded", HomeData.init)
