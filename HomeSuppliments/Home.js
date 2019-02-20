467064b9dba99722a650ee983af334d12f7e623d
croz0034 <32398078+croz0034@users.noreply.github.com>
2019-02-19 19:58:21 -0500

    tip gramma fix

--- HomeSuppliments/Home.js
+++ HomeSuppliments/Home.js
@@ -312,7 +312,7 @@ const Tips = [
 "Great weapons with 18” of heavy padding (4” cross section) gain the armour breaking and shield crushing traits regardless of class using them. (P18)",
 "Gift of air is susceptible to magic, or melee armour breaking or shield crushing. Consult your local Barbarian (p33, 58)",
 "All effects from 'poison’ are considered magical. A protection from magic, or void touched could save your life (p49)",
-"you can frozen or insubstantial to become immune to more dangerous spells, lord T calls this 'shadow dancing’ (p51, 52)",
+"you can use frozen or insubstantial to become immune to more dangerous spells, lord T calls this 'shadow dancing’ (p51, 52)",
 "magic requires a free hand to cast. Luckily a small shield does not need to be held in hand to be wielded (p19, 49)",
 "Fight after death, elemental barrage, and sanctuary are all ongoing effects. In the right circumstances a greater / release can do wonders (p51)",
 
